d0053272d39c44aab53baff9a2588392ede0352b
sadikneipp <sadi.neto13@gmail.com>
2019-03-25 14:45:35 -0300

    Updates

---
+++ precache-manifest.1212ed44ce62b429742ad50f9cb82d5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sadikneipp.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7bd2b9cf7dde7bd82cf4",
+    "url": "/sadikneipp.github.io/static/js/runtime~main.1824efa5.js"
+  },
+  {
+    "revision": "132b3885970efca2db43",
+    "url": "/sadikneipp.github.io/static/js/main.453cdddd.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/sadikneipp.github.io/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "132b3885970efca2db43",
+    "url": "/sadikneipp.github.io/static/css/main.d069cc2b.chunk.css"
+  },
+  {
+    "revision": "0bfdc9ae7d65e4a9e07ef942a2e8ca32",
+    "url": "/sadikneipp.github.io/index.html"
+  }
+];
